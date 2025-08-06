9842f802bea0033fce531d4a565e1f4e07b966a3
Tamara Ricardo <161865777+datos-ine@users.noreply.github.com>
2025-08-06 10:23:42 -0300

    Update 06-08-2025

--- cleaning_defun.R
+++ cleaning_defun.R
@@ -6,7 +6,6 @@
 ### https://datos.gob.ar/dataset/salud-defunciones-generales-mensuales-ocurridas-registradas-republica-argentina
 ### Autora: Tamara Ricardo
 ### Fecha modificación:
-# Tue Aug  5 16:33:53 2025 ------------------------------
 
 
 # Cargar paquetes ---------------------------------------------------------
@@ -14,7 +13,7 @@ pacman::p_load(
   rio,
   janitor,
   tidyverse
-  )
+)
 
 
 # Cargar datos ------------------------------------------------------------
@@ -37,9 +36,9 @@ tabyl(defun_raw$region)
 
 tabyl(defun_raw$jurisdiccion)
 
-tabyl(defun_raw$grupo_etario)
-
 tabyl(defun_raw$sexo_nombre)
+
+tabyl(defun_raw$grupo_etario)
 
 tabyl(defun_raw$grupo_causa_defuncion_CIE10)
 
@@ -65,7 +64,6 @@ tabyl(covid_raw$fallecido)
 defun <- defun_raw |> 
   # Estandarizar nombres de columnas
   rename(sexo = sexo_nombre,
-         prov_id = jurisdiccion,
          cie10_grupo = grupo_causa_defuncion_CIE10,
          cie10_cod = cod_causa_muerte_CIE10) |> 
   
@@ -80,18 +78,8 @@ defun <- defun_raw |>
   
   # Modificar etiquetas región
   mutate(region = fct_relabel(
-    region, ~ c("Centro", "NEA", "NOA1", "NOA", "NOA2", "Cuyo1", "Cuyo2", 
+    region, ~ c("Centro", "NEA", "NOA1", "NOA", "NOA2", "Cuyo1", "Cuyo2",
                 "Patagonia Norte", "Patagonia Sur")
-    )) |> 
-  
-  # Crear columna para id de provincia
-  mutate(prov_id = str_remove_all(prov_id, "[^0-9]")) |> 
-  
-  # Crear variable para nombre de provincia
-  mutate(prov_nombre = fct_relabel(
-    prov_id, ~ c("Córdoba", "Corrientes", "CABA", "Chaco", "Entre Ríos",
-                 "Formosa", "Mendoza", "Misiones", "Buenos Aires",
-                 "Santa Fe", "Tucumán", NA)
   )) |> 
   
   # Modificar etiquetas grupo etario
@@ -100,35 +88,54 @@ defun <- defun_raw |>
                       "60-69 años", "70-79 años", "80+ años")
   )) |> 
   
-  # Crear columna para causas de muerte
+  # Crear variable para id numérico de provincia
+  mutate(prov_id = str_extract(jurisdiccion, "\\d+")) |> 
+  
+  # Crear variable para etiqueta de provincia
+  mutate(prov_nombre = case_when(
+    prov_id == "14" ~ "Córdoba",
+    prov_id == "30" ~ "Entre Ríos",
+    prov_id == "6" ~ "Buenos Aires",
+    prov_id == "90" ~ "Tucumán",
+    prov_id == "99" ~ NA,
+    .default = str_remove_all(jurisdiccion, "[0-9]|\\.")
+  )) |> 
+  
+  # Crear variable para causas de muerte
   mutate(causa_def = case_when(
-    # Tumores malignos (C00:C99)
-    cie10_grupo == "0200 T MALIGNOS" ~ "Cáncer",
     # Diabetes mellitus (E10:E14)
-    cie10_grupo == "0300 DIABETES MELLITUS" ~ "Diabetes",
-    # Respiratorias crónicas (J30:J98)
-    between(cie10_cod, "J30", "J98") ~ "Enf. respiratorias crónicas",
-    # Hipertensión arterial (I10:I13)
-    between(cie10_cod, "I10", "I15") ~ "Hipertensión",
+    between(cie10_cod, "E10", "E14") ~ "Diabetes mellitus",
+    # Tumores malignos (C00:C99)
+    between(cie10_cod, "C00", "C99") ~ "Cáncer",
+    # Respiratorias crónicas (J40:J47)
+    between(cie10_cod, "J40", "J47") ~ "Enf. respiratorias crónicas",
+    # Alzheimer y demencias (F00, F01, F03, G30:G31)
+    cie10_cod %in% c("F00", "F01", "F03", "G30", "G31") ~ "Alzheimer/Demencias",
+    # Enfermedad renal crónica (N18)
+    cie10_cod == "N18" ~ "Enf. renal crónica",
+    # Enfermedad de Parkinson (G20)
+    cie10_cod == "G20" ~ "Enf. de Parkinson",
+    # Accidente cerebrovascular - ACV (I60:I69)
+    between(cie10_cod, "I60", "I69") ~ "ACV",
     # Enfermedad cardíaca isquémica (I20:I25)
     between(cie10_cod, "I20", "I25") ~ "Enf.cardíaca isquémica",
-    # ACV (I60:I69)
-    between(cie10_cod, "I60", "I69") ~ "ACV",
+    # Hipertensión arterial (I10:I13)
+    between(cie10_cod, "I10", "I15") ~ "Hipertensión",
     # Otras enfermedades cardiovasculares
     between(cie10_cod, "I00", "I09")|
       between(cie10_cod, "I14", "I19")|
       between(cie10_cod, "I26", "I59")|
-      between(cie10_cod, "I70", "I99") ~ "Otras cardiovasculares",
-    # Alzheimer y demencias (F00, F01, F03, G30:G31)
-    cie10_cod %in% c("F00", "F01", "F03", "G30", "G31") ~ "Alzheimer/Demencias",
-    # Parkinson (G20)
-    cie10_cod == "G20" ~ "Parkinson",
+      between(cie10_cod, "I70", "I99") ~ "Enf. cardiovasculares",
+    # COVID-19
+    cie10_cod == "U07" ~ "COVID-19",
+    # Otras causas
     .default = "Otra"
-    )) |> 
+  )) |> 
   
-  # Agrupar datos
-  count(anio_def, mes_def, region, prov_id, prov_nombre, grupo_etario, sexo, causa_def,
-        wt = cantidad)
+  # Agrupar por causa
+  count(region, prov_id, prov_nombre, anio_def, mes_def,
+        sexo, grupo_etario, causa_def,
+        wt = cantidad, name = "n_def")
 
 
 ### Explorar datos
@@ -142,8 +149,8 @@ tabyl(defun$causa_def)
 
 
 
-# Limpieza datos defunciones COVID-19 -------------------------------------
-defun_covid <- covid_raw |> 
+# Limpieza datos COVID-19 -------------------------------------------------
+datos_covid <- covid_raw |> 
   
   # Estandarizar nombres de columnas
   rename(prov_id = residencia_provincia_id,
@@ -161,21 +168,12 @@ defun_covid <- covid_raw |>
   # Filtrar datos menores de edad
   filter(edad_años_meses == "Años" & edad >= 20) |> 
   
-  # Filtrar casos no fallecidos
-  filter(fallecido == "SI") |> 
-  
-  # Seleccionar columnas relevantes
-  select(id_evento_caso, sexo, edad, prov_id, prov_nombre, fecha_fallecimiento) |> 
-  
   # Modificar etiquetas sexo
   mutate(sexo = fct_relabel(sexo, ~ c("femenino", "masculino"))) |> 
   
-  # Modificar formato fecha fallecimiento
-  mutate(fecha_fallecimiento = ymd(fecha_fallecimiento)) |> 
-  
-  # Crear variables para año y mes de defunción
-  mutate(anio_def = year(fecha_fallecimiento),
-         mes_def = month(fecha_fallecimiento)) |> 
+  # Modificar formato fechas 
+  mutate(across(.cols = starts_with("fecha"),
+                .fns = ~ ymd(.x))) |> 
   
   # Crear variable para grupo etario
   mutate(grupo_edad = case_when(
@@ -186,17 +184,6 @@ defun_covid <- covid_raw |>
     between(edad, 70, 79) ~ "70-79 años",
     .default = "80+ años"
   )) |> 
-  
-  # Contar fallecimientos por año, mes, provincia, grupo etario y sexo
-  count(anio_def, mes_def, prov_id, prov_nombre, grupo_edad, sexo,
-        name = "defun_covid") |> 
-  
-  # Añadir filas faltantes (combinaciones sin defunciones)
-  complete(nesting(anio_def, mes_def), 
-           nesting(prov_id, prov_nombre),
-           grupo_edad, sexo,
-           fill = list(defun_covid = 0)
-  ) |> 
   
   # Crear variable para región
   mutate(region = case_when(
@@ -210,6 +197,26 @@ defun_covid <- covid_raw |>
     prov_nombre %in% c("Chubut", "Santa Cruz", "Tierra del Fuego") ~ "Patagonia Sur",
     .default = "Centro"
   ), .after = prov_nombre)
+  
+
+### Crear dataset de fallecidos COVID-19
+defun_covid <- datos_covid |> 
+  # Filtrar no fallecidos
+  filter(fallecido == "SI") |> 
+  
+  # Crear variables para año y mes de defunción
+  mutate(anio_def = year(fecha_fallecimiento),
+         mes_def = month(fecha_fallecimiento)) |> 
+  
+  # Agrupar fallecimientos por COVID-19
+  count(region, prov_id, prov_nombre, anio_def, mes_def,
+        grupo_edad, sexo, name = "n_def_covid") |> 
+  
+  # Completar filas faltantes
+  complete(nesting(region, prov_id, prov_nombre),
+           anio_def, mes_def,
+           grupo_edad, sexo,
+           fill = list(n_def_covid = 0))
 
 
 
